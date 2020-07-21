ab42ca5d3dea95f1adb6e49da9bf4123dbb26008
Jose Velez <jose-0919@hotmail.com>
2020-07-21 21:26:06 +0000

    we finish let's check

--- _printf.c
+++ _printf.c
@@ -8,47 +8,42 @@ int _printf(const char *format, ...)
 {
 int i = 0;
 int p;
+int counter = 0;
 va_list valist;
 
 va_start(valist, format);
 while (format != NULL && *format != '\0')
 {
-if (*format == '%')
+	if (*format == '%')
 	{
 	format++;
-
 	switch (format[i])
 	{
 		case 'c':
 		p = print_c(valist);
 		format++;
 		break;
-
 		case 's':
 		p = print_s(valist);
 		format++;
 		break;
-
+		return (p);
 		case '%':
 		p = print_percentage();
 		format++;
 		break;
-
 		case 'd':
 		p = print_number(valist);
 		format++;
 		break;
-
 		case 'i':
 		p = print_number(valist);
 		format++;
 		break;
 	}
+}	else
+	{ write(1, &(*format++), 1);
 	}
-	else
-	{
-			write(1, &(*format++), 1);
-	}
-}
-return (p);
+	counter++;
+} return (counter);
 }
