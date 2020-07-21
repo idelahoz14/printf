f73abf600da10cdeaa77cc9e6e589003f2616347
Idelahoz <idelahzo14@cuc.edu.co>
2020-07-21 02:15:28 +0000

    function itoa

---
+++ _itoa.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * _itoa - reverses a string.
+ * @num: numbers
+ * @base:base 10
+ * Return: Always 0.
+ */
+char *_itoa(long int num, int base)
+{
+	static char *array = "0123456789abcdef";
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = num;
+
+	if (num < 0)
+	{
+		n = -num;
+		sign = '-';
+	}
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	do      {
+		*--ptr = array[n % base];
+		n /= base;
+	} while (n != 0);
+
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
+}
