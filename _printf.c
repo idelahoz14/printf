60e9602d1b6f3d4ccd1049a62339c1484da79704
Jose Velez <jose-0919@hotmail.com>
2020-07-21 02:53:48 +0000

    My first body of printf

--- _printf.c
+++ _printf.c
@@ -1,18 +1,39 @@
 #include "holberton.h"
 /**
-*
-*
-*
+* _printf - my file printf
+* @format: my prototype
+* Return: 0 always
 */
 int _printf(const char *format, ...)
 {
+int i = 0;
+int p;
+va_list valist;
 
-	options func[] = 
+va_start(valist, format);
+while (format != NULL && *format != '\0')
+{
+if (*format == '%')
 	{
-		{"%c", print_char}
-		{"%c", print_c},
-		{"%s", print_s},
-		{"%%", print_percentage},
-		{"%d", print_d_i},
+	format++;
+
+	switch (format[i])
+	{
+		case 'c':
+		p = print_c(valist);
+		format++;
+		break;
+
+		case 's':
+		p = print_s(valist);
+		format++;
+		break;
 	}
+	}
+	else
+	{
+			write(1, &(*format++), 1);
+	}
+}
+return (0);
 }
