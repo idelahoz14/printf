4021d6655f4a89757d422108418704b9fb5d0673
Jose Velez <jose-0919@hotmail.com>
2020-07-21 03:07:47 +0000

    Try operations

---
+++ operations.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+* print_c - characters
+* @valist: list
+* Return: it return stdout
+*/
+int  print_c(va_list valist)
+{
+		int c = va_arg(valist, int);
+			write(1, &(c), 1);
+		return (1);
+}
+/**
+* print_s - characters
+* @valist: list
+* Return: it return stdout
+*/
+int print_s(va_list valist)
+{
+	int counter = 0;
+	char *s = va_arg(valist, char *);
+
+	while (*s)
+	{
+		write(1, &(*s++), 1);
+		counter++;
+	}
+	return (counter);
+}
