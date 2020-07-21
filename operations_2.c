90b8486825a8fb19690c1a94e0ba2d07ba566076
Idelahoz <idelahzo14@cuc.edu.co>
2020-07-21 15:23:40 +0000

    file that contains more functions

---
+++ operations_2.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * print_show - It prints to stdout.
+ * @c: Char to print.
+ * Return: It returns to stdout.
+ */
+int print_show(int c)
+{
+	return (write(1, &c, 1));
+}
+/**
+ * print_percentage - It prints to stdout.
+ * Return: It returns to stdout.
+ */
+int print_percentage(void)
+{
+	print_show('%');
+	return (1);
+}
+/**
+ * print_number - print decimal int
+ * @ap: argument
+ * Return: i
+ */
+int print_number(va_list valist)
+{
+	long int p, i;
+	char *str;
+
+	p = va_arg(valist, int);
+	str = _itoa(n, 10);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		print_show(str[i]);
+	}
+	return (i);
+}
