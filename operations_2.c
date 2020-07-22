cee725177e6b645397dda3d43b45dceb32927575
Idelahoz <idelahzo14@cuc.edu.co>
2020-07-22 03:04:12 +0000

    organizing functions

--- operations_2.c
+++ operations_2.c
@@ -1,23 +1,5 @@
 #include "holberton.h"
 /**
- * print_show - It prints to stdout.
- * @c: Char to print.
- * Return: It returns to stdout.
- */
-int print_show(int c)
-{
-	return (write(1, &c, 1));
-}
-/**
- * print_percentage - It prints to stdout.
- * Return: It returns to stdout.
- */
-int print_percentage(void)
-{
-	print_show('%');
-	return (1);
-}
-/**
  * print_number - print decimal int
  * @ap: argument
  * Return: i
